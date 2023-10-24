db535006115440d8c45a287867db4bc82b92d9d6
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2023-10-24 20:32:59 -0300

    ajustes de estilo

--- App.js
+++ App.js
@@ -8,12 +8,12 @@ export default function App() {
         <Text style={styles.titulo}>LISTAS</Text>
       </View>
 
-      <BoxComponente corFundo={"#FFA4A4"} corTexto= {"#C62222"} texto={"Animais"}/>
-      <BoxComponente corFundo={"#EDA4FF"} corTexto= {"#7422C6"} texto={"Carros"}/>
-      <BoxComponente corFundo={"#A4D4FF"} corTexto= {"#2277C6"} texto={"Peixes"}/>
-      <BoxComponente corFundo={"#A4FFC3"} corTexto= {"#22C646"} texto={"Times"}/>
-      <BoxComponente corFundo={"#D1D6D3"} corTexto= {"#434A44"} texto={"Filmes"}/>
-      <BoxComponente corFundo={"#E8EFBD"} corTexto= {"#747D10"} texto={"Linguagens"}/>
+        <BoxComponente corFundo={"#FFA4A4"} corTexto= {"#C62222"} texto={"Animais"}/>
+        <BoxComponente corFundo={"#EDA4FF"} corTexto= {"#7422C6"} texto={"Carros"}/>
+        <BoxComponente corFundo={"#A4D4FF"} corTexto= {"#2277C6"} texto={"Peixes"}/>
+        <BoxComponente corFundo={"#A4FFC3"} corTexto= {"#22C646"} texto={"Times"}/>
+        <BoxComponente corFundo={"#D1D6D3"} corTexto= {"#434A44"} texto={"Filmes"}/>
+        <BoxComponente corFundo={"#E8EFBD"} corTexto= {"#747D10"} texto={"Linguagens"}/>
     </View>
   )
 }
--- components/Box/styes.js
+++ components/Box/styes.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     listaOrigem:{
      height: 107,
-     width:408,
+     width:'95%',
      justifyContent: 'center',
      marginTop: 15,
      alignItems: 'center',
